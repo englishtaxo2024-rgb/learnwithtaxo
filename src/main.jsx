1ff6cfebc54e370cf7af21faef4c912565cc9001
englishtaxo2024-rgb <englishtaxo2024@gmail.com>
2026-06-08 02:43:44 +0300

    Route placement test to dedicated experience

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { PlacementExperience } from './pages/PlacementExperience.jsx';
 import { LanguageProvider } from './i18n/LanguageProvider.jsx';
 import { installHomepageEnhancements } from './homepageEnhancements.js';
 import './styles/index.css';
@@ -10,11 +11,14 @@ import './styles/hero-poster.css';
 
 installHomepageEnhancements();
 
+const placementPaths = new Set(['/placement-test', '/student/placement']);
+const RootExperience = placementPaths.has(window.location.pathname) ? PlacementExperience : App;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LanguageProvider>
       <BrowserRouter>
-        <App />
+        <RootExperience />
       </BrowserRouter>
     </LanguageProvider>
   </React.StrictMode>
